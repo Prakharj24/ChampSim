9e5bc1f74079deb7ad3a30e4f21d71a6e6b8d97c
Prakhar Jawre <jprakhar@cse.iitk.ac.in>
2018-05-03 18:48:43 +0530

    scripts updated

--- run_8sim.sh
+++ run_8sim.sh
@@ -4,15 +4,14 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/prakhar/ChampSim/DRAMSim2/
 binary=${1}
 n_warm=${2}
 n_sim=${3}
-option=${5}
-temp_num=${4}
-
+start=${4}
+end=${5}
 mkdir -p results_8core
 
-while [ "$temp_num" -le 78 ]
+while [ "$start" -le "$end" ]
 do
-	num=$temp_num
-	while [ "$num" -le `expr $temp_num + 8`  ] && [ "$num" -le 78 ];
+	num=$start
+	while [ "$num" -le `expr $start + 8`  ] && [ "$num" -le "$end" ];
 	do
             echo "Running benchmark ${num}!"
             trace1=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $1}'`
@@ -24,10 +23,10 @@ do
             trace7=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $7}'`
             trace8=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $8}'`
 
-            (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz ${TRACE_DIR}/${trace3}.trace.gz ${TRACE_DIR}/${trace4}.trace.gz ${TRACE_DIR}/${trace5}.trace.gz ${TRACE_DIR}/${trace6}.trace.gz ${TRACE_DIR}/${trace7}.trace.gz ${TRACE_DIR}/${trace8}.trace.gz) &> results_8core/mix${num}-${binary}${option}.txt &
+            (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz ${TRACE_DIR}/${trace3}.trace.gz ${TRACE_DIR}/${trace4}.trace.gz ${TRACE_DIR}/${trace5}.trace.gz ${TRACE_DIR}/${trace6}.trace.gz ${TRACE_DIR}/${trace7}.trace.gz ${TRACE_DIR}/${trace8}.trace.gz) &> results_8core/mix${num}-${binary}.txt &
 		num=`expr $num + 1`
 	done
 wait ${!}
-temp_num=$num
+start=$num
 echo "set finished!"
 done
--- run_sim.sh
+++ run_sim.sh
@@ -3,24 +3,23 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/prakhar/ChampSim/DRAMSim2/
 binary=${1}
 n_warm=${2}
 n_sim=${3}
-option=${5}
-temp_num=${4}
+start=${4}
+end=${5}
+mkdir -p results_4core
 
-mkdir -p results_mix_4core
-
-while [ "$temp_num" -le 201 ]
+while [ "$start" -le "$end" ]
 do
-	num=$temp_num
-	while [ "$num" -le `expr $temp_num + 8`  ] && [ "$num" -le 201 ];
+	num=$start
+	while [ "$num" -le `expr $start + 8`  ] && [ "$num" -le "$end" ];
 	do
 		trace1=`sed -n ''$num'p' sim_list/4core_workloads.txt | awk '{print $1}'`
 		trace2=`sed -n ''$num'p' sim_list/4core_workloads.txt | awk '{print $2}'`
 		trace3=`sed -n ''$num'p' sim_list/4core_workloads.txt | awk '{print $3}'`
 		trace4=`sed -n ''$num'p' sim_list/4core_workloads.txt | awk '{print $4}'`
 
-		(./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz ${TRACE_DIR}/${trace3}.trace.gz ${TRACE_DIR}/${trace4}.trace.gz) &> results_mix_4core/mix${num}-${binary}${option}.txt &
+		(./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz ${TRACE_DIR}/${trace3}.trace.gz ${TRACE_DIR}/${trace4}.trace.gz) &> results_4core/mix${num}-${binary}.txt &
 		num=`expr $num + 1`
 	done
 wait ${!}
-temp_num=$num
+start=$num
 done
