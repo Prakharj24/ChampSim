a7fafe11271e433c0be835d7b99ea71446eea115
Prakhar Jawre <jprakhar@cse.iitk.ac.in>
2018-05-03 18:18:13 +0530

    8 core script and sim list added

---
+++ run_8sim.sh
@@ -0,0 +1,33 @@
+
+TRACE_DIR=/home/prakhar/traces
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/prakhar/ChampSim/DRAMSim2/
+binary=${1}
+n_warm=${2}
+n_sim=${3}
+option=${5}
+temp_num=${4}
+
+mkdir -p results_8core
+
+while [ "$temp_num" -le 78 ]
+do
+	num=$temp_num
+	while [ "$num" -le `expr $temp_num + 8`  ] && [ "$num" -le 78 ];
+	do
+            echo "Running benchmark ${num}!"
+            trace1=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $1}'`
+            trace2=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $2}'`
+            trace3=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $3}'`
+            trace4=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $4}'`
+            trace5=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $5}'`
+            trace6=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $6}'`
+            trace7=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $7}'`
+            trace8=`sed -n ''$num'p' sim_list/8core_workloads.txt | awk '{print $8}'`
+
+            (./bin/${binary} -warmup_instructions ${n_warm}000000 -simulation_instructions ${n_sim}000000 ${option} -traces ${TRACE_DIR}/${trace1}.trace.gz ${TRACE_DIR}/${trace2}.trace.gz ${TRACE_DIR}/${trace3}.trace.gz ${TRACE_DIR}/${trace4}.trace.gz ${TRACE_DIR}/${trace5}.trace.gz ${TRACE_DIR}/${trace6}.trace.gz ${TRACE_DIR}/${trace7}.trace.gz ${TRACE_DIR}/${trace8}.trace.gz) &> results_8core/mix${num}-${binary}${option}.txt &
+		num=`expr $num + 1`
+	done
+wait ${!}
+temp_num=$num
+echo "set finished!"
+done
